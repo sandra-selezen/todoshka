29b14f128c7b3c4133104de930eda2715f430369
Oleksandra Selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-07-23 19:08:04 +0300

    Updated Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,30 @@
+"use client";
+
+import { fetchTodos } from "@/redux/features/operations";
+import { AppDispatch, useAppSelector } from "@/redux/store";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
 export default function Home() {
+
+  const todos = useAppSelector(state => state.todos.items);
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    dispatch(fetchTodos());
+  }, [dispatch]);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>TODO application</h1>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>
+            <h3>{todo.title}</h3>
+            <p>{todo.description}</p>
+          </li>
+        ))}
+      </ul>
     </main>
   )
 }
--- src/redux/features/operations.ts
+++ src/redux/features/operations.ts
@@ -1,7 +1,9 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-axios.defaults.baseURL = "https://todoshka-rest-api.onrender.com/api";
+// axios.defaults.baseURL = "https://todoshka-rest-api.onrender.com/api";
+
+axios.defaults.baseURL = "https://64bd08662320b36433c75a8a.mockapi.io/api";
 
 interface Todo {
   id: string;
