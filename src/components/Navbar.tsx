dec16f435c02e71ff5d8445e4df2b4084a807ace
Oleksandra Selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-07-24 19:06:00 +0300

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='mx-auto p-8 bg-fuchsia-300 w-full flex flex-row justify-between items-center'>
+    <nav className='mx-auto p-8 bg-fuchsia-300 w-full flex flex-row justify-between items-center font-bold text-fuchsia-950'>
       <ul className='flex items-center gap-6 flex-row'>
         <li>Home</li>
         <li>Todo List</li>
