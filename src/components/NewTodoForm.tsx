c6dec3e66eae811995a844143897275319bfe427
Oleksandra Selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-07-30 21:16:10 +0300

    Update NewTodoForm.tsx

--- src/components/NewTodoForm.tsx
+++ src/components/NewTodoForm.tsx
@@ -13,7 +13,7 @@ const initialValues: INewTodo = {
 const NewTodoForm = () => {
   return (
     <>
-      <h2>Add New Task</h2>
+      <h2 className='mb-6 font-bold text-3xl'>Add New Task</h2>
       <Formik
         initialValues={initialValues}
         validationSchema={newTodoSchema}
@@ -24,12 +24,12 @@ const NewTodoForm = () => {
           }, 1000);
         }}
       >
-        <Form>
-          <label htmlFor='title'>Title</label>
-          <Field id='title' name='title' />
-          <label htmlFor='description'>Description</label>
-          <Field as='textarea' id='description' name='description' />
-          <button type='submit'>Add</button>
+        <Form className='flex flex-col w-2/5 gap-4'>
+          <label htmlFor='title' className='after:content-["*"] after:ml-0.5 after:text-red-500'>Title</label>
+          <Field id='title' name='title' placeholder='Enter task title' className='bg-inherit border-2 rounded-xl p-2.5 outline-transparent' />
+          <label htmlFor='description' className='after:content-["*"] after:ml-0.5 after:text-red-500'>Description</label>
+          <Field as='textarea' id='description' name='description' row='5' placeholder='Enter task description' className='bg-inherit border-2 rounded-xl p-2.5 outline-transparent' />
+          <button type='submit'  className='font-bold border-2 rounded-xl py-2.5 px-3.5 mx-auto hover:bg-[rgba(248,252,251,0.3)]'>Add New Task</button>
         </Form>
       </Formik>
     </>
