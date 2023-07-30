d728be39ab5cbe350cd62607e7680ff95d8a4f51
Oleksandra Selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-07-30 21:09:21 +0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className='px-6 py-8 text-center shadow-[0px_-10px_15px_rgba(17,63,103,0.6)] rounded-t-3xl'>
       <div>
-        <p>&#169; 2023 | All Rights Reserved | Developed with 💙💛 by Sandra Selezen</p>
+        <p>Developed with 💙💛 by Sandra Selezen</p>
       </div>
     </footer>
   )
