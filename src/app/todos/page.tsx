51489b4c92828c4254931e757759e05110581e89
Oleksandra Selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-08-01 16:25:06 +0300

    Updated TodoList component

--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -3,11 +3,11 @@
 import { TodoList } from '@/components'
 import { AppDispatch, useAppSelector } from '@/redux/store'
 import { fetchTodos } from '@/redux/todo/operations';
+import Link from 'next/link';
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux';
 
 const Todos = () => {
-
   const todos = useAppSelector(state => state.todos.items);
   const dispatch = useDispatch<AppDispatch>();
 
@@ -17,7 +17,10 @@ const Todos = () => {
 
   return (
     <>
-      <h1 className='page-title'>Todo List</h1>
+      <div className='flex flex-row justify-between items-center mb-5'>
+        <h1 className='page-title'>Todo List</h1>
+        <Link href='/todos/new' className='todo__button'>New task</Link>
+      </div>
       <TodoList todos={todos} />
     </>
   )
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -4,6 +4,8 @@ import { AppDispatch } from '@/redux/store';
 import { deleteTodo, toggleChecked } from '@/redux/todo/operations';
 import { ITodo } from '@/types/todo'
 import React from 'react'
+import { toast } from 'react-hot-toast';
+import { MdDeleteForever } from "react-icons/md";
 import { useDispatch } from 'react-redux';
 
 interface IProps {
@@ -15,18 +17,32 @@ const TodoList = ({ todos }: IProps) => {
   const dispatch = useDispatch<AppDispatch>();
 
   return (
-    <div>
-      <ul className='grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-        {todos.map(todo => (
-          <li key={todo._id}>
-            <h2>{todo.title}</h2>
-            <p>{todo.description}</p>
-            <input type='checkbox' checked={todo.checked} onChange={() => dispatch(toggleChecked(todo))} />
-            <button type='button' onClick={() => dispatch(deleteTodo(todo._id))}>Delete</button>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ul className='grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+      {todos.map(todo => (
+        <li
+          key={todo._id}
+          className='todo__item'
+        >
+          <div>
+            <h2 className='todo__title'>{todo.title}</h2>
+            <p className='todo__text'>{todo.description}</p>
+          </div>
+          <div className='todo__actions-container'>
+            <input type='checkbox' className='accent-pink-500' checked={todo.checked} onChange={() => dispatch(toggleChecked(todo))} />
+            <button
+              type='button'
+              aria-label='Delete'
+              title='Delete'
+              onClick={() => {
+                dispatch(deleteTodo(todo._id));
+                toast.success('Task successfully deleted!');
+              }}>
+              <MdDeleteForever />
+            </button>
+          </div>
+        </li>
+      ))}
+    </ul>
   )
 }
 
